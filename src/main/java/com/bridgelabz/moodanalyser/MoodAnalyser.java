3f9c92b8ff862fb80d606aad6c9c0fa443d3049e
Archanagowda23 <archanar249@gmail.com>
2022-04-03 19:29:47 +0530

    initial commit

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.moodanalyser;
+
+/**
+ * purpose :Given a Message, ability to analyse and respond Happy or Sad Mood
+ *@author : Archana
+ * since : 03-04-2022
+ */
+
+public class MoodAnalyser {
+}
