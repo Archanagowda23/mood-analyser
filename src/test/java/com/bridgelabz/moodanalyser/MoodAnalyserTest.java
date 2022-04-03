5718f53c66d35a5d3ab820d85b460f450e82f3fc
Archanagowda23 <archanar249@gmail.com>
2022-04-03 19:35:29 +0530

    Added ability to analyse sad

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,5 +1,16 @@
 package com.bridgelabz.moodanalyser;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// Test case to analyze sad mood of the user
 public class MoodAnalyserTest {
 
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodanalyser=new MoodAnalyser();
+        String actualResult=moodanalyser.analyseMood("User is Sad");
+        Assertions.assertEquals("Sad",actualResult);
+    }
 }
+
