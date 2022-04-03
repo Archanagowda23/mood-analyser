958054cf33fd47aad864aaf92bd872d1549498fc
Archanagowda23 <archanar249@gmail.com>
2022-04-03 20:07:46 +0530

    Added ability to analyse happy or sad mood to handle exception

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -6,13 +6,27 @@ import org.junit.jupiter.api.Test;
 // Test case to analyze sad mood of the user
 public class MoodAnalyserTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        Assertions.assertTrue(true);
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Sad Mood");
+        String actualResult = moodanalyser.analyseMood();
+        Assertions.assertEquals("Sad", actualResult);
+    }
+
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in happy Mood");
+        String actualResult = moodanalyser.analyseMood();
+        Assertions.assertEquals("Happy", actualResult);
+    }
+
+    @Test
+    public void givenMessage_Null_ShouldReturnExceptionHandled() {
+        MoodAnalyser moodanalyser = new MoodAnalyser(null);
+        String actualResult = moodanalyser.analyseMood();
+        Assertions.assertEquals("Exception Handled", actualResult);
     }
 }
+
 
 
